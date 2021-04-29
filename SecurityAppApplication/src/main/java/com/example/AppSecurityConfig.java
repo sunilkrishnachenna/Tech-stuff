e8a4e9b2b0f54285d747ee7505e43caa78d66a8e
Sunil <Sunil@DESKTOP-LDV6CAI.lan>
2021-04-29 14:22:41 -0500

    New Security APP Application

---
+++ SecurityAppApplication/src/main/java/com/example/AppSecurityConfig.java
@@ -0,0 +1,64 @@
+package com.example;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+
+
+@SuppressWarnings("deprecation")
+@Configuration
+@EnableWebSecurity
+public class AppSecurityConfig  extends WebSecurityConfigurerAdapter{
+@Autowired
+	private UserDetailsService userDetailsService;
+
+
+
+@Override
+protected void configure(HttpSecurity http) throws Exception {
+	http
+	.csrf().disable()
+	.authorizeRequests().antMatchers("/login").permitAll()
+	.anyRequest().authenticated()
+	.and()
+	.formLogin()
+	.loginPage("/login").permitAll()
+	.and()
+	.logout().invalidateHttpSession(true)
+	.clearAuthentication(true)
+	.logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
+	.logoutSuccessUrl("/logout-success").permitAll();
+}
+
+
+
+@Bean
+	public AuthenticationProvider authProvider()
+	{
+	DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
+	provider.setUserDetailsService(userDetailsService);
+	provider.setPasswordEncoder(NoOpPasswordEncoder.getInstance());
+	return provider;
+	
+	}
+	/*
+	 * @Bean
+	 * 
+	 * @Override protected UserDetailsService userDetailsService() {
+	 * 
+	 * List<UserDetails> users = new ArrayList<>();
+	 * users.add(User.withDefaultPasswordEncoder().username("sunny").password("1234"
+	 * ).roles("USER").build());
+	 * 
+	 * return new InMemoryUserDetailsManager(users); }
+	 */
+}
